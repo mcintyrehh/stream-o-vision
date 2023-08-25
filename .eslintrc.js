9eceafd2d1de9bc187e819e1dff802ddcd018cb7
Mcintyre, Henry Holleran <henrym@gatech.edu>
2023-08-25 18:44:49 -0400

    Next steps in getting a ts-node server up and running

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 // @see: https://eslint.org/docs/latest/use/configure/configuration-files
 module.exports = {
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest", // Allows the use of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
+    parser: "@typescript-eslint/parser",
   },
+  plugins: [
+    "@typescript-eslint", // Enables eslint-plugin-typescript
+  ],
   extends: ["plugin:@typescript-eslint/recommended"], // Uses the linting rules from @typescript-eslint/eslint-plugin
   env: {
     node: true, // Enable Node.js global variables
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-function sayMyName(name: string): void {
-    if (name === "Heisenberg") {
-      console.log("You're right 👍");
-    } else {
-      console.log("You're wrong 👎");
-    }
-  }
-   
-  sayMyName("Heisenberg");
+import express from "express";
+const app = express();
+// import fs
+import fs from "fs";
+ 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+ 
+app.listen(3000);

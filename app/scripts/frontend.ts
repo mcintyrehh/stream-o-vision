92a8c2ce8f4892741535062b3322559b6bbab242
Henry McIntyre <mcintyrehh@gmail.com>
2024-05-12 12:14:34 -0400

    Spaces -> tabs

--- app/scripts/frontend.ts
+++ app/scripts/frontend.ts
@@ -2,116 +2,109 @@ type VolumeDirection = 'up' | 'down'
 
 const socket = new WebSocket('ws://localhost:3000');
 socket.onopen = () => {
-  socket.send('Hello, its ya boy Henry');
+	socket.send('Hello, its ya boy Henry');
 }
 
 socket.onmessage = (event) => handleWSMessage(event.data);
 
 const handleWSMessage = (data: string) => {
-  console.log("~henry - data: ", data)
-  const [_, sensor, reading] = data.split(':');
-  switch (sensor) {
-    case 'channel':
-      console.log("~henry - channel: ", reading)
-      setChannel(parseInt(reading));
-      break; 
-    case 'volume':
-      console.log("~henry - volume direction: ", reading)
-      setVolume(reading as VolumeDirection);
-      break; 
-    case 'mute':
-      console.log("~henry - muting")
-      setMuted();
-      break; 
-    default:
-      return;
-  }
+	console.log("~henry - data: ", data)
+	const [_, sensor, reading] = data.split(':');
+	switch (sensor) {
+		case 'channel':
+			console.log("~henry - channel: ", reading)
+			setChannel(parseInt(reading));
+			break; 
+		case 'volume':
+			console.log("~henry - volume direction: ", reading)
+			setVolume(reading as VolumeDirection);
+			break; 
+		case 'mute':
+			console.log("~henry - muting")
+			setMuted();
+			break; 
+		default:
+			return;
+	}
 }
 
 type Stream = {
-  streamUrl: string,
-  url: string,
-  name: string,
-  channelNumber: number,
+	streamUrl: string,
+	url: string,
+	name: string,
+	channelNumber: number,
 }
 
 //TODO set up webpack so I can import this from a const file
 const streams: Stream[] = [
-  {
-    streamUrl:
-      "https://videos3.earthcam.com/fecnetwork/20489.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/empirestatebuilding/?cam=esb37",
-    name: "Empire State Building",
-    channelNumber: 0,
-  },
-  {
-    streamUrl: "https://videos3.earthcam.com/fecnetwork/3983.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/brooklynbridge/?cam=gzcchd",
-    name: "Brooklyn Bridge",
-    channelNumber: 1,
-  },
-  {
-    streamUrl:
-      "https://videos3.earthcam.com/fecnetwork/10874.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/worldtradecenter/?cam=skyline_g",
-    name: "World Trade Center",
-    channelNumber: 2,
-  },
-  {
-    streamUrl:
-      "https://videos-3.earthcam.com/fecnetwork/22640.flv/playlist.m3u8?",
-    url: "https://www.earthcam.com/usa/newyork/highline/?cam=highline",
-    name: "High Line Cam",
-    channelNumber: 3,
-  },
-  {
-    streamUrl:
-      "https://videos3.earthcam.com/fecnetwork/hdtimes10.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo1",
-    name: "Times Square",
-    channelNumber: 4,
-  },
-  {
-    streamUrl:
-      "https://videos3.earthcam.com/fecnetwork/1415.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/cams/newyork/timessquare/?cam=tstwo_hd",
-    name: "Times Square View (South)",
-    channelNumber: 5,
-  },
-  {
-    streamUrl:
-      "https://videos3.earthcam.com/fecnetwork/15559.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/cams/newyork/timessquare/?cam=tsrobo3",
-    name: "Times Square Crossroads",
-    channelNumber: 8,
-  },
-  {
-    streamUrl: "https://videos3.earthcam.com/fecnetwork/9974.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/cams/newyork/timessquare/?cam=tsstreet",
-    name: "Times Square Street Cam",
-    channelNumber: 7,
-  },
-  {
-    streamUrl:
-      "https://videos-3.earthcam.com/fecnetwork/23505.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/heraldsquare/?cam=heraldsquare_nyc",
-    name: "Times Square Crossroads",
-    channelNumber: 8,
-  },
-  {
-    streamUrl:
-      "https://videos3.earthcam.com/fecnetwork/13903.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/midtown/skyline/?cam=midtown4k",
-    name: "Midtown Skyline",
-    channelNumber: 9,
-  },
-  {
-    streamUrl:
-      "https://videos-3.earthcam.com/fecnetwork/4089.flv/playlist.m3u8",
-    url: "https://www.earthcam.com/usa/newyork/midtown/skyline/?cam=midtown4k",
-    name: "Statue of Liberty",
-    channelNumber: 10,
-  },
+	{
+		streamUrl:
+			"https://videos3.earthcam.com/fecnetwork/20489.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/usa/newyork/empirestatebuilding/?cam=esb37",
+		name: "Empire State Building",
+		channelNumber: 0,
+	},
+	{
+		streamUrl: "https://videos3.earthcam.com/fecnetwork/3983.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/usa/newyork/brooklynbridge/?cam=gzcchd",
+		name: "Brooklyn Bridge",
+		channelNumber: 1,
+	},
+	{
+		streamUrl:
+			"https://videos3.earthcam.com/fecnetwork/10874.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/usa/newyork/worldtradecenter/?cam=skyline_g",
+		name: "World Trade Center",
+		channelNumber: 2,
+	},
+	{
+		streamUrl:
+			"https://videos-3.earthcam.com/fecnetwork/22640.flv/playlist.m3u8?",
+		url: "https://www.earthcam.com/usa/newyork/highline/?cam=highline",
+		name: "High Line Cam",
+		channelNumber: 3,
+	},
+	{
+		streamUrl:
+			"https://videos3.earthcam.com/fecnetwork/hdtimes10.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo1",
+		name: "Times Square",
+		channelNumber: 4,
+	},
+	{
+		streamUrl:
+			"https://videos3.earthcam.com/fecnetwork/1415.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/cams/newyork/timessquare/?cam=tstwo_hd",
+		name: "Times Square View (South)",
+		channelNumber: 5,
+	},
+	{
+		streamUrl:
+			"https://videos-3.earthcam.com/fecnetwork/15559.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/cams/newyork/timessquare/?cam=tsrobo3",
+		name: "Times Square Crossroads",
+		channelNumber: 6,
+	},
+	{
+		streamUrl: "https://videos3.earthcam.com/fecnetwork/9974.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/cams/newyork/timessquare/?cam=tsstreet",
+		name: "Times Square Street Cam",
+		channelNumber: 7,
+	},
+	{
+		streamUrl:
+			"https://videos3.earthcam.com/fecnetwork/13903.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/usa/newyork/midtown/skyline/?cam=midtown4k",
+		name: "Midtown Skyline",
+		channelNumber: 8,
+	},
+	{
+		streamUrl:
+			"https://videos-3.earthcam.com/fecnetwork/4089.flv/playlist.m3u8",
+		url: "https://www.earthcam.com/usa/newyork/midtown/skyline/?cam=midtown4k",
+		name: "Statue of Liberty",
+		channelNumber: 9,
+	},
 ];
 
 //@ts-ignore
@@ -121,142 +114,142 @@ let currentChannelIndex = 2;
 let muted = true;
 
 const proxyURLFromStreamIndex = (streamIndex: number) => {
-  const proxy_url = "http://127.0.0.1:8182";
-  const referer_url = "https://www.earthcam.com/";
-  const file_extension = ".m3u8";
+	const proxy_url = "http://127.0.0.1:8182";
+	const referer_url = "https://www.earthcam.com/";
+	const file_extension = ".m3u8";
 
-  return `${proxy_url}/${btoa(
-    `${streams[streamIndex].streamUrl}|${referer_url}`,
-  )}${file_extension}`;
+	return `${proxy_url}/${btoa(
+		`${streams[streamIndex].streamUrl}|${referer_url}`,
+	)}${file_extension}`;
 };
 
 const setChannel = (channel: number) => {
-  currentChannelIndex = channel;
-  createHLSVideoElement();
+	currentChannelIndex = channel;
+	createHLSVideoElement();
 }
 
 // @todo: https://github.com/mcintyrehh/stream-o-vision/issues/1
 const setVolume = (volume: VolumeDirection) => {
-  // @todo: change this to logarithmic!
-  // @see: https://github.com/videojs/video.js/issues/8498
-  const volumeDiff = volume === 'up' ? .01 : -.01
-  _video.volume += volumeDiff;
-  console.log("volume: ", _video.volume)
-  const videoEl = document.getElementsByTagName('video')[0];
-  const textTrack = videoEl.textTracks[0];
-  const activeCues = textTrack.activeCues || []
-  for (let i = 0; i < activeCues?.length || 0; i++) {
-    textTrack.removeCue(activeCues[i])
-  }
-  textTrack.addCue(new VTTCue(videoEl.currentTime, videoEl.currentTime + 2, `Volume: ${Math.round(videoEl.volume * 100)}`))
+	// @todo: change this to logarithmic!
+	// @see: https://github.com/videojs/video.js/issues/8498
+	const volumeDiff = volume === 'up' ? .01 : -.01
+	_video.volume += volumeDiff;
+	console.log("volume: ", _video.volume)
+	const videoEl = document.getElementsByTagName('video')[0];
+	const textTrack = videoEl.textTracks[0];
+	const activeCues = textTrack.activeCues || []
+	for (let i = 0; i < activeCues?.length || 0; i++) {
+		textTrack.removeCue(activeCues[i])
+	}
+	textTrack.addCue(new VTTCue(videoEl.currentTime, videoEl.currentTime + 2, `Volume: ${Math.round(videoEl.volume * 100)}`))
 }
 
 const setMuted = () => {
-  console.log("~henry - muting")
-  muted = !muted;
-  _video.muted = muted;
+	console.log("~henry - muting")
+	muted = !muted;
+	_video.muted = muted;
 }
 
 const addTextTrackToVideoElement = (videoEl: HTMLVideoElement) => {
-  console.log(videoEl.currentTime);
-  const { channelNumber, name } = streams[currentChannelIndex];
-  let track = videoEl.addTextTrack("captions", "Channel Info", "en-US");
-  track.mode = "showing";
-  track.addCue(new VTTCue(0, 10, createChannelCueText(channelNumber, name)))
+	console.log(videoEl.currentTime);
+	const { channelNumber, name } = streams[currentChannelIndex];
+	let track = videoEl.addTextTrack("captions", "Channel Info", "en-US");
+	track.mode = "showing";
+	track.addCue(new VTTCue(0, 10, createChannelCueText(channelNumber, name)))
 }
 
 const onChannelChange = (direction: "up" | "down") => {
-  const channelDirection = direction === "up" ? 1 : -1;
-  // Adding streams.length makes sure wrap-around works for negative numbers
-  currentChannelIndex =
-    (currentChannelIndex + channelDirection + streams.length) % streams.length;
-  hls.loadSource(proxyURLFromStreamIndex(currentChannelIndex))
+	const channelDirection = direction === "up" ? 1 : -1;
+	// Adding streams.length makes sure wrap-around works for negative numbers
+	currentChannelIndex =
+		(currentChannelIndex + channelDirection + streams.length) % streams.length;
+	hls.loadSource(proxyURLFromStreamIndex(currentChannelIndex))
 };
 
 const createChannelCueText = (channelNumber: Stream['channelNumber'], channelDescription: Stream['name']) => {
-  return `Channel ${channelNumber} - ${channelDescription}`
+	return `Channel ${channelNumber} - ${channelDescription}`
 }
 
 const createHLSVideoElement = () => {
-  var wrapper = document.createElement("div");
-  wrapper.className = "video-wrapper";
-  
+	var wrapper = document.createElement("div");
+	wrapper.className = "video-wrapper";
+	
 
-  var video = document.createElement("video");
-  video.style.height = "100%";
-  video.style.width = "100%";
-  video.style.backgroundImage = "url('./static/static-tv-static.gif')"
+	var video = document.createElement("video");
+	video.style.height = "100%";
+	video.style.width = "100%";
+	video.style.backgroundImage = "url('./static/static-tv-static.gif')"
 
-  wrapper.appendChild(video);
-  // Global ref to video elem
-  _video = video;
-  // @ts-ignore
-  window._video = video;
-  // creating info divs below the video
-  var videoInfo = document.createElement("div");
-  var channelNumber = document.createElement("span");
-  channelNumber.className = "channel-div";
-  channelNumber.innerHTML = `Channel ${streams[currentChannelIndex].channelNumber}`;
-  videoInfo.appendChild(channelNumber);
-  var videoName = document.createElement("span");
-  videoName.innerHTML = ` - ${streams[currentChannelIndex].name}`;
-  videoInfo.appendChild(videoName);
-  wrapper.appendChild(videoInfo);
+	wrapper.appendChild(video);
+	// Global ref to video elem
+	_video = video;
+	// @ts-ignore
+	window._video = video;
+	// creating info divs below the video
+	var videoInfo = document.createElement("div");
+	var channelNumber = document.createElement("span");
+	channelNumber.className = "channel-div";
+	channelNumber.innerHTML = `Channel ${streams[currentChannelIndex].channelNumber}`;
+	videoInfo.appendChild(channelNumber);
+	var videoName = document.createElement("span");
+	videoName.innerHTML = ` - ${streams[currentChannelIndex].name}`;
+	videoInfo.appendChild(videoName);
+	wrapper.appendChild(videoInfo);
 
-  // creating channel up and down buttons
-  const channelDown = document.createElement("button");
-  channelDown.innerHTML = "Channel Down";
-  channelDown.onclick = () => onChannelChange("down");
-  wrapper.appendChild(channelDown);
+	// creating channel up and down buttons
+	const channelDown = document.createElement("button");
+	channelDown.innerHTML = "Channel Down";
+	channelDown.onclick = () => onChannelChange("down");
+	wrapper.appendChild(channelDown);
 
-  const channelUp = document.createElement("button");
-  channelUp.innerHTML = "Channel Up";
-  channelUp.onclick = () => onChannelChange("up");
-  wrapper.appendChild(channelUp);
+	const channelUp = document.createElement("button");
+	channelUp.innerHTML = "Channel Up";
+	channelUp.onclick = () => onChannelChange("up");
+	wrapper.appendChild(channelUp);
 
-  // creating volume up and down buttons
-  const volumeDown = document.createElement("button");
-  volumeDown.innerHTML = "Volume Down";
-  volumeDown.onclick = () => setVolume("down");
-  wrapper.appendChild(volumeDown);
+	// creating volume up and down buttons
+	const volumeDown = document.createElement("button");
+	volumeDown.innerHTML = "Volume Down";
+	volumeDown.onclick = () => setVolume("down");
+	wrapper.appendChild(volumeDown);
 
-  const volumeUp = document.createElement("button");
-  volumeUp.innerHTML = "Volume Up";
-  volumeUp.onclick = () => setVolume("up");
-  wrapper.appendChild(volumeUp);
+	const volumeUp = document.createElement("button");
+	volumeUp.innerHTML = "Volume Up";
+	volumeUp.onclick = () => setVolume("up");
+	wrapper.appendChild(volumeUp);
 
-  const muted = document.createElement("button");
-  muted.innerHTML = "Toggle Mute";
-  muted.onclick = () => setMuted();
-  wrapper.appendChild(muted);
+	const muted = document.createElement("button");
+	muted.innerHTML = "Toggle Mute";
+	muted.onclick = () => setMuted();
+	wrapper.appendChild(muted);
 
-  const body = document.getElementsByTagName("body")[0];
-  body.appendChild(wrapper);
+	const body = document.getElementsByTagName("body")[0];
+	body.appendChild(wrapper);
 
-  //@ts-ignore
-  if (Hls.isSupported()) {
-    //@ts-ignore
-    hls = new Hls();
-    console.log("hls: ", hls);
-    hls.loadSource(proxyURLFromStreamIndex(currentChannelIndex));
-    hls.attachMedia(video);
-    // @ts-ignore
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      video.muted = true;
-      video.play();
-      addTextTrackToVideoElement(video);
-    });
-  }
-  // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
-  // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
-  // This is using the built-in support of the plain video element, without using hls.js.
-  else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-    video.src = "https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8";
-    video.addEventListener("canplay", function () {
-      video.muted = true;
-      video.play();
-    });
-  }
+	//@ts-ignore
+	if (Hls.isSupported()) {
+		//@ts-ignore
+		hls = new Hls();
+		console.log("hls: ", hls);
+		hls.loadSource(proxyURLFromStreamIndex(currentChannelIndex));
+		hls.attachMedia(video);
+		// @ts-ignore
+		hls.on(Hls.Events.MANIFEST_PARSED, function () {
+			video.muted = true;
+			video.play();
+			addTextTrackToVideoElement(video);
+		});
+	}
+	// hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+	// When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element through the `src` property.
+	// This is using the built-in support of the plain video element, without using hls.js.
+	else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+		video.src = "https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8";
+		video.addEventListener("canplay", function () {
+			video.muted = true;
+			video.play();
+		});
+	}
 };
 
 createHLSVideoElement();
--- app/src/main.ts
+++ app/src/main.ts
@@ -11,34 +11,34 @@ console.log("os.type(): ", os.type())
 const portPath = os.type() === "Windows_NT" ? "COM3" : "/dev/ttyACM0"
 
 SerialPort.list().then(ports => {
-  console.log("Ports: ", ports);
+	console.log("Ports: ", ports);
 })
 // Open WebSocket for communication with frontend
 const wss = new WebSocketServer({port: 3000})
 
 wss.on('connection', (socket) => {
-  console.log("WebSocket Connected")
-  const port = new SerialPort({
-    path: portPath,
-    baudRate: 115200,
-  })
-  
-  // Open errors will be emitted as an error event
-  port.on('error', function(err) {
-    console.log('Error: ', err.message)
-  })
-  
-  port.on("open", () => {
-    console.log("Serial Port Open")
-    port.on('data', (data) => {
-      console.log("serial port data: ", data.toString())
-      socket.send(data.toString());
-    })
-  })
-  
-  socket.on('message', (message) => {
-    console.log("message: ", message.toString())
-  })
+	console.log("WebSocket Connected")
+	const port = new SerialPort({
+		path: portPath,
+		baudRate: 115200,
+	})
+	
+	// Open errors will be emitted as an error event
+	port.on('error', function(err) {
+		console.log('Error: ', err.message)
+	})
+	
+	port.on("open", () => {
+		console.log("Serial Port Open")
+		port.on('data', (data) => {
+			console.log("serial port data: ", data.toString())
+			socket.send(data.toString());
+		})
+	})
+	
+	socket.on('message', (message) => {
+		console.log("message: ", message.toString())
+	})
 })
 // console.log("wssSocket: ", wssSocket);
 
@@ -47,36 +47,36 @@ wss.on('connection', (socket) => {
 // Spin up HLS proxy server to get around CORS/Origin, Referer HTTP request headers
 console.log("Starting HLS proxy server...");
 exec("echo 'henry is cool'", (error, stdout, stderr) => {
-  console.log(stdout);
+	console.log(stdout);
 });
 exec("pwd", (error, stdout, stderr) => {
-  console.log("henry");
-  console.log("iscool");
-  console.log(stdout);
+	console.log("henry");
+	console.log("iscool");
+	console.log(stdout);
 });
 
 exec(
-  `node ./node_modules/@warren-bank/hls-proxy/hls-proxy/bin/hlsd.js --port "8182"`,
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`node subprocess error.message: ${error.message}`);
-      console.error(`node subprocess error: ${error}`);
-      return;
-    }
+	`node ./node_modules/@warren-bank/hls-proxy/hls-proxy/bin/hlsd.js --port "8182"`,
+	(error, stdout, stderr) => {
+		if (error) {
+			console.error(`node subprocess error.message: ${error.message}`);
+			console.error(`node subprocess error: ${error}`);
+			return;
+		}
 
-    if (stderr) {
-      console.error(`node subprocess stderr: ${stderr}`);
-      return;
-    }
-    console.log(`node subprocess stdout: ${stdout}`);
-  },
+		if (stderr) {
+			console.error(`node subprocess stderr: ${stderr}`);
+			return;
+		}
+		console.log(`node subprocess stdout: ${stdout}`);
+	},
 );
 
 app.get("/", function (req, res) {
-  console.log("dirname: ", __dirname);
-  const pathway = path.join(__dirname, "..", "..", "app", "public", "index.html");
-  console.log("pathway: ", pathway);
-  res.sendFile(pathway);
+	console.log("dirname: ", __dirname);
+	const pathway = path.join(__dirname, "..", "..", "app", "public", "index.html");
+	console.log("pathway: ", pathway);
+	res.sendFile(pathway);
 });
 
 app.use(express.static("public"));
@@ -84,5 +84,5 @@ app.use("/scripts", express.static(path.join(__dirname, "..", "scripts/")));
 app.use("/static", express.static(path.join(__dirname, "..", "static/")));
 
 app.listen(1337, () => {
-	require('child_process').exec('start http://localhost:1337');
+		require('child_process').exec('start http://localhost:1337');
 });
